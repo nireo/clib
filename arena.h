c0427caea943f63eeb17f528880e353b535ce7eb
nireo <eemil.lehtikangas1@gmail.com>
2025-10-05 20:14:19 +0300

    removed the arena source file

--- arena.h
+++ arena.h
@@ -5,13 +5,18 @@
 #include <stdlib.h>
 #include <string.h>
 
+// allocate `n` objects of type `t` from arena `a`.
+// aborts if out of memory.
 #define new(a, t, n) (t *)alloc(a, sizeof(t), _Alignof(t), n)
 
+// a simple arena allocator.
 typedef struct {
   char *beg;
   char *end;
 } arena;
 
+// allocate `count` objects of `size` bytes with `align` alignment from arena
+// `a`.
 static inline void *alloc(arena *a, ptrdiff_t size, ptrdiff_t align,
                           ptrdiff_t count) {
   ptrdiff_t padding = -(uintptr_t)a->beg & (align - 1);
@@ -24,6 +29,7 @@ static inline void *alloc(arena *a, ptrdiff_t size, ptrdiff_t align,
   return memset(p, 0, count * size);
 }
 
+// create a new arena with capacity `cap` bytes.
 static inline arena new_arena(ptrdiff_t cap) {
   arena a = {0};
   a.beg = (char *)malloc(cap);
